e81fe0a27b970cfb036a9a1a25853e5ab168539e
KESHAV KUMAR <keshavkumark03@gmail.com>
2024-06-19 13:07:27 +0530

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,24 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
-    description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    title: "Web Design",
+    description: "A project showcasing various web design techniques and practices.",
+    url: "https://github.com/NARAINDRAGOKUL/web-design",
   },
   {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    title: "Object -Detection",
+    description: "A object detection model using YOLO v3 .",
+    url: "https://github.com/NARAINDRAGOKUL/Object-detection-project",
   },
   {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    title: "Any Time Electricity Bill Payment",
+    description: "A project designed for easy and convenient electricity bill payments at any time.",
+    url: "https://github.com/NARAINDRAGOKUL/Object-detection-project",
   },
   {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    title: "Portfolio Website",
+    description: "A personal portfolio website showcasing my skills, projects, and experience.",
+    url: "https://keshavkumar03.github.io/microsoft-learn/",
   },
 ];
 
